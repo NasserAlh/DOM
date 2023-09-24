e1d4b42697c05a5e223839a999b7929fb36443b8
Nasser H <nassq8hh@gmail.com>
2023-09-24 14:52:43 +0300

    Order Book Imbalance (OBI) Analysis

---
+++ src/main/java/day3/DepthOfMarket.java
@@ -0,0 +1,13 @@
+package day3;
+
+import velox.api.layer1.annotations.*;
+import velox.api.layer1.common.Log;
+import velox.api.layer1.data.InstrumentInfo;
+import velox.api.layer1.simplified.*;
+
+@Layer1SimpleAttachable
+@Layer1StrategyName("DepthOfMarket")
+@Layer1ApiVersion(Layer1ApiVersionValue.VERSION2)
+public class DepthOfMarket implements CustomModuleAdapter {
+
+}
