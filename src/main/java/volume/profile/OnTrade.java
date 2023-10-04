0ecf0e075e2aa40429813a8bf91d24ff9884fc08
Nasser H <nassq8hh@gmail.com>
2023-10-04 11:07:18 +0300

    Volume Profile

--- src/main/java/volume/profile/OnTrade.java
+++ src/main/java/volume/profile/OnTrade.java
@@ -8,15 +8,16 @@ import velox.api.layer1.simplified.InitialState;
 import velox.api.layer1.data.InstrumentInfo;
 import velox.api.layer1.common.Log;
 import velox.api.layer1.simplified.TradeDataListener;
+
 import javax.swing.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 @Layer1SimpleAttachable
-@Layer1StrategyName("VP ZEG")
+@Layer1StrategyName("VP from VS Code")
 @Layer1ApiVersion(Layer1ApiVersionValue.VERSION1)
-public class TradeDataTest implements TradeDataListener, CustomModule {
+public class OnTrade implements TradeDataListener, CustomModule {
 
-    private final ConcurrentHashMap<Double, Integer> volumeProfile = new ConcurrentHashMap<>();
+    private final ConcurrentSkipListMap<Double, Integer> volumeProfile = new ConcurrentSkipListMap<>();
     private VolumeProfilePanel volumeProfilePanel;
     private JFrame frame;
 
@@ -25,7 +26,13 @@ public class TradeDataTest implements TradeDataListener, CustomModule {
         SwingUtilities.invokeLater(() -> {
             frame = new JFrame("Volume Profile");
             volumeProfilePanel = new VolumeProfilePanel(volumeProfile);
-            frame.add(volumeProfilePanel);
+
+            JScrollPane scrollPane = new JScrollPane(volumeProfilePanel);
+            scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+            frame.add(scrollPane);
+
             frame.setSize(400, 400);
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             frame.setVisible(true);
@@ -43,6 +50,6 @@ public class TradeDataTest implements TradeDataListener, CustomModule {
     public void onTrade(double price, int size, TradeInfo tradeInfo) {
         volumeProfile.merge(price, size, Integer::sum);
         Log.info("Updated Volume Profile: " + volumeProfile.toString());
-        SwingUtilities.invokeLater(() -> volumeProfilePanel.updateVolumeProfile(new ConcurrentHashMap<>(volumeProfile)));
+        SwingUtilities.invokeLater(() -> volumeProfilePanel.updateVolumeProfile(new ConcurrentSkipListMap<>(volumeProfile)));
     }
 }
--- src/main/java/volume/profile/VolumeProfilePanel.java
+++ src/main/java/volume/profile/VolumeProfilePanel.java
@@ -2,7 +2,10 @@ package volume.profile;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class VolumeProfilePanel extends JPanel {
     private static final int START_X = 50;
@@ -10,46 +13,72 @@ public class VolumeProfilePanel extends JPanel {
     private static final int BAR_HEIGHT = 20;
     private static final int PADDING = 50;
 
-    private ConcurrentHashMap<Double, Integer> volumeProfile;
+    private ConcurrentSkipListMap<Double, Integer> volumeProfile;
+    private double zoomFactor = 1.0;
+    private static final double ZOOM_INCREMENT = 0.1;
 
-    public VolumeProfilePanel(ConcurrentHashMap<Double, Integer> volumeProfile) {
+    public VolumeProfilePanel(ConcurrentSkipListMap<Double, Integer> volumeProfile) {
         this.volumeProfile = volumeProfile;
+        setPreferredSize(new Dimension(400, (int)((BAR_HEIGHT + 10) * volumeProfile.size() * zoomFactor)));
+
+        addMouseWheelListener(e -> {
+            if (e.getWheelRotation() < 0) {
+                zoomFactor += ZOOM_INCREMENT;
+            } else {
+                zoomFactor = Math.max(0.7, zoomFactor - ZOOM_INCREMENT);
+            }
+            repaint();
+        });
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        int x = START_X;
-        int y = START_Y;
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        List<Double> sortedPrices = new ArrayList<>(volumeProfile.keySet());
+        Collections.sort(sortedPrices, Collections.reverseOrder());
+
+        int xForBars = START_X + 20;
+        int xForPrice = START_X;
+        int y = (int)(START_Y * zoomFactor);
 
         int maxVolume = volumeProfile.values().stream().max(Integer::compare).orElse(1);
-        int maxBarWidth = this.getWidth() - x - PADDING;
+        int maxBarWidth = this.getWidth() - xForBars - PADDING;
 
-        for (Double price : volumeProfile.keySet()) {
+        for (Double price : sortedPrices) {
             int rawWidth = volumeProfile.get(price);
             int normalizedWidth = (int) ((rawWidth / (double) maxVolume) * maxBarWidth);
 
-            g.setColor(Color.BLUE);
-            g.fillRect(x, y, normalizedWidth, BAR_HEIGHT);
+            float ratio = rawWidth / (float) maxVolume;
+            Color gradientColor = new Color(0, 0, (int)(255 * ratio));
+            g.setColor(gradientColor);
+            g.fillRect(xForBars, y, normalizedWidth, BAR_HEIGHT);
 
             String volumeText = String.valueOf(rawWidth);
             FontMetrics fm = g.getFontMetrics();
             int textWidth = fm.stringWidth(volumeText);
-            int textX = x + (normalizedWidth - textWidth) / 2;
+            int textX = xForBars + (normalizedWidth - textWidth) / 2;
             int textY = y + (BAR_HEIGHT - fm.getHeight()) / 2 + fm.getAscent();
 
             g.setColor(Color.WHITE);
             g.drawString(volumeText, textX, textY);
 
             g.setColor(Color.BLACK);
-            g.drawString(String.format("%.2f", price * .25), x - 40, y + BAR_HEIGHT / 2 + 5);
+            g.fillRect(xForPrice - 50, y, 45, BAR_HEIGHT);
 
-            y += BAR_HEIGHT + 10;
+            g.setColor(Color.WHITE);
+            g.drawString(String.format("%.2f", price * 0.25), xForPrice - 40, y + BAR_HEIGHT / 2 + 5);
+
+            y += (int)((BAR_HEIGHT + 10) * zoomFactor);
         }
     }
 
-    public void updateVolumeProfile(ConcurrentHashMap<Double, Integer> newVolumeProfile) {
+    public void updateVolumeProfile(ConcurrentSkipListMap<Double, Integer> newVolumeProfile) {
         this.volumeProfile = newVolumeProfile;
+        setPreferredSize(new Dimension(400, (int)((BAR_HEIGHT + 10) * volumeProfile.size() * zoomFactor)));
+        revalidate();
         repaint();
     }
 }
